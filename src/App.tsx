e30a0b24987343a57aa902f008c6951c2a603464
U-ADC\h.mufidah <ADC+h.mufidah@XX-HPMJ1Z2-L.aad.iongroup.net>
2022-10-30 13:06:21 +0700

    update dependenxies

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import Directory from "./components/Directory";
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <Directory />
+    </div>
+  );
+};
+
+export default App;
