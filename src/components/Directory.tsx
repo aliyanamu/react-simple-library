0138c3b77293506cd101bdf7d1d8ad2a5a9cb7a6
U-ADC\h.mufidah <ADC+h.mufidah@XX-HPMJ1Z2-L.aad.iongroup.net>
2022-10-30 13:44:02 +0700

    include image to declaration to load in src

--- src/components/Directory.tsx
+++ src/components/Directory.tsx
@@ -1,10 +1,11 @@
 import React from "react";
+import logo from "../images/logo.svg";
 
 const Directory: React.FC = () => {
   return (
     <>
       <header className="App-header">
-        <img src="/images/logo.svg" className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
